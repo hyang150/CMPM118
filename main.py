70343a572a7f0a13e2e3432030f1e28001d54ef3
Hanwen Yang <hyang150@ucsc.edu>
2023-01-25 15:44:02 -0800

    totally finished nose

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 ## Importing Necessary Modules
 import shutil  # to save it locally
 import requests  # to get image from the web
+import os #use OS lib to create
+import json
 
+#Global variables
+Nose_Dictionary = {"Nose" : []}
+Ear_Dictionary = {"Ear":[]}
+Eye_Dictionary = {"Eye":[]}
 """
 Original Url:
 https://preview.bitmoji.com/avatar-builder-v3/preview/hair?
@@ -50,22 +56,32 @@ image_url_Ori = "https://preview.bitmoji.com/avatar-builder-v3/preview/hair?" \
                 "&pupil=2152" \
                 "&top=54"
 
-
 def nose_replace():
 	"""
 	This is the nose replace funtion
-	it will replace all the nose on the default faces from the link above
+	it will replace all the nose on the default faces from orginal link
 
 	:return: it does not return anything, but it will generate the files
 	"""
-	for i in range(0, 30000):# The range here could be changed to check every number.
-		str_nose_replace = str("nose=" + str(i));
+	try:
+		os.mkdir('result');
+	except FileExistsError:
+		print("folder already exists");
+	
+	try:
+		os.mkdir("result/nose_result");
+	except FileExistsError:
+		print("folder already exists");
+	
+	for i in range(1434, 2000):# The range here could be changed to check every number.
+		str_nose_replace = str("nose=" + str(i)); #replace the string in the url
 		print(str_nose_replace);
 		str_image_text_Dealed = image_url_Ori.replace("nose=1491", str_nose_replace);
 		r = requests.get(str_image_text_Dealed, stream=True);
 		if r.status_code == 200:
-			print("success at nose case", i);
-			output_file_name = "default_face" + "nose" + str(i) + ".png";
+			print("success get nose case", i);
+			Nose_Dictionary["Nose"].append(i); #update the list
+			output_file_name = "result/nose_result/"+"default_face" + "_nose_" + str(i) + ".png";
 			# Set decode_content value to True, otherwise the downloaded image file's size will be zero.
 			r.raw.decode_content = True;
 			# Open a local file with wb ( write binary ) permission.
@@ -76,7 +92,12 @@ def nose_replace():
 			print("bad request code for nose ", i);
 		else:
 			print('Image Couldn\'t be retreived');
-
+	Nose_json_data = json.dumps(Nose_Dictionary)
+	Json_file = "Nose.json"
+	with open(Json_file,"wb") as f:
+		f.write(Nose_json_data);
+		print("succssful create the Json of Nose")
+			
 
 if __name__ == "__main__":
 	# Check if the image was retrieved successfully
